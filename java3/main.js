4ff45c7cb63f1caa3ec30067a910b33bda7b8c8f
Maxsafarov1 <128669573+Maxsafarov1@users.noreply.github.com>
2023-06-15 18:13:39 +0300

    Update main.js

--- java3/main.js
+++ java3/main.js
@@ -2,14 +2,10 @@
     function f() {
         let A = prompt('Введите число A:'),
             B = prompt('Введите число B:');
-        {
-            function div(val, by) {
-                return (val - val % by) / by;
-            }
 
             if (A % B == 0)
                 alert('A является делителем числа B')
             else (B % A == 0)
-                alert('B не является делителем числа А')
+            alert('B не является делителем числа А')
         }
-    }}
+    }
